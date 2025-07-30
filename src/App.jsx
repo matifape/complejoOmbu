02ec4acc65bc2cc1c821701d0aaf693e0bea886f
matifape <matiasalmasia1@gmail.com>
2025-07-30 19:26:12 -0300

    cambios componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Contact from "./components/Contact";
+
 import Cabs from "./components/Cabs";
 import Servicios from "./components/Servicios";
 import Maps from "./components/Maps";
 import Footer from "./components/Footer";
+import PruebaCab from "./components/PruebaCab";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     <>
       <Header />
       <Hero />
-      <Cabs />
+      <PruebaCab/>
       <Servicios />
       <About />
       <Maps/>
